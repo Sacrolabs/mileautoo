d4824966605fd78cc8f12ccea55d6b43750c9cde
Kishore Saha <sahakishore4@gmail.com>
2021-11-03 23:03:11 +0530

    heroku publish

--- pages/_document.js
+++ pages/_document.js
@@ -18,10 +18,7 @@ class MyDocument extends Document {
             <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
             
-            {/* <link rel="stylesheet" href="./assets/css/quote.css"/>
-            <link rel="stylesheet" href="./assets/css/about.css"/>
-            <link rel="stylesheet" href="./assets/css/account.css"/>
-            <link rel="stylesheet" href="./assets/css/chatbot.css"/> */}
+           
         </Head>
         <body>
           <Main />
